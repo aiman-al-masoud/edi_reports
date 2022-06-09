29073d2c407bf554bcabc3ab79928be6cd8105ff
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-09 19:56:42 +0200

    ...

--- lab3/assignment-3/experiment2/scripton.sh
+++ lab3/assignment-3/experiment2/scripton.sh
@@ -3,6 +3,7 @@ WEBSITE='https://web.unipv.it'
 NUM_REQ_COORDS="131 750"
 FINISH_COORDS="549 750"
 CACHE_CHECK_BOX_COORDS="978 507"
+REPEAT_EXPERIMENT=2 # number of times experiment is repeated
 
 # copy some text from a text-field at the specified coordinates
 function copyFromScreen(){
@@ -14,17 +15,21 @@ function copyFromScreen(){
     echo ""
 }
 
-# reload the page and get the data from the screen
+# reload the page and get the data from the screen for $1 times
 function runExperiment(){
-    xdotool key F5 # reload
-    sleep 3 
-    # get number of requests (bottom left)
-    copyFromScreen $NUM_REQ_COORDS
-    xdotool key Ctrl+Shift+E # re-open network monitor (just in case)
-    # get PLT ('Finish')
-    copyFromScreen $FINISH_COORDS
-    xdotool key Ctrl+Shift+E # re-open network monitor (just in case)
-    sleep 2   
+    for (( c=0; c<$1; c++ ))
+        do 
+            echo "experiment "$c
+            xdotool key F5 # reload
+            sleep 3 
+            # get number of requests (bottom left)
+            copyFromScreen $NUM_REQ_COORDS
+            xdotool key Ctrl+Shift+E # re-open network monitor (just in case)
+            # get PLT ('Finish')
+            copyFromScreen $FINISH_COORDS
+            xdotool key Ctrl+Shift+E # re-open network monitor (just in case)
+            sleep 2  
+        done
 }
 
 # toggle the cache checkbox on firefox assuming network monitor is open
@@ -45,10 +50,10 @@ sleep 2
 
 # assume cache is enabled (checkbox UNticked)
 echo '----------with cache----------'
-runExperiment
+runExperiment $REPEAT_EXPERIMENT
 # turn cache off and repeat experiment
 toggleCache
 echo '----------without cache----------'
-runExperiment
+runExperiment $REPEAT_EXPERIMENT
 # turn cache back on
 toggleCache 
