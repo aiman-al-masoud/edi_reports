7d16be16218102879184ebf7ca354a1716bac00c
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-09 21:25:02 +0200

    ...

--- lab3/assignment-3/experiment2/scripts/main.sh
+++ lab3/assignment-3/experiment2/scripts/main.sh
@@ -2,25 +2,25 @@ N=5 # repeats
  
 # Mainly HTTP1
 
-./scripton.sh  https://web.unipv.it/ $N  >> http1.txt
+./scripton.sh  https://web.unipv.it/ $N  >> dump.txt
 
-./scripton.sh  https://www.pythonanywhere.com/ $N >> http1.txt
+./scripton.sh  https://www.pythonanywhere.com/ $N >> dump.txt
 
-./scripton.sh  https://www.mit.edu/ $N >> http1.txt
+./scripton.sh  https://www.mit.edu/ $N >> dump.txt
 
 # Mainly HTTP2
 
-./scripton.sh  https://github.com/ $N >> http2.txt 
+./scripton.sh  https://github.com/ $N >> dump.txt 
 
-./scripton.sh https://archive.org/ $N >> http2.txt
+./scripton.sh https://archive.org/ $N >> dump.txt
 
-./scripton.sh https://stackoverflow.com/ $N >> http2.txt
+./scripton.sh https://stackoverflow.com/ $N >> dump.txt
 
 
 # Unsecure (HTTP1)
 
-./scripton.sh  http://maxgames.com/ $N >> unsecure.txt 
+./scripton.sh  http://maxgames.com/ $N >> dump.txt 
 
-./scripton.sh  http://go.com/ $N >> unsecure.txt 
+./scripton.sh  http://go.com/ $N >> dump.txt 
 
-./scripton.sh  http://mclab.unipv.it/ $N >> unsecure.txt 
+./scripton.sh  http://mclab.unipv.it/ $N >> dump.txt 
